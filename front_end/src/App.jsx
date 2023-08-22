04dbfee0bff3abed5a130dfa51ab8a872658dad1
MartinLahoumh <martinlahoumh103102@gmail.com>
2023-08-22 19:24:32 -0400

    Revert "crash"
    This reverts commit ad71c249bf99f642dfc46b3472b2b91c5d764ec5.

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,18 +1,24 @@
 import "./css/App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import SignIn from "./Routes/SignIn";
 import Home from "./Routes/Home";
 import ErrorPage from "./Routes/ErrorPage";
-import Header from "./components/Header";
-import SignUp from "./Routes/SignUp";
+
 function App() {
   return (
     <Router>
       <div className="App">
+        <header>
+          {/* replace p with logo */}
+          <p>Shopping Center</p>
+          <div className="sections">
+            {/* place everything else in header into here */}
+            <Link to="/SignIn">Sign In</Link>
+          </div>
+        </header>
         <Routes>
           <Route exact path="/SignIn" element={<SignIn />}></Route>
-          <Route exact path="/SignUp" element={<SignUp />}></Route>
-          <Route path="/:auth" element={<Home />}></Route>
           <Route path="/" element={<Home />}></Route>
           <Route path="*" element={<ErrorPage />}></Route>
         </Routes>
--- front_end/src/Routes/Home.jsx
+++ front_end/src/Routes/Home.jsx
@@ -1,28 +1,22 @@
-import { useParams } from "react-router";
-import Header from "../components/Header";
 function Home() {
-  let {auth} = useParams();
-  console.log(auth);
-    return (
-      <>
-        <div>
-          <Header auth={auth}/>
-          {console.log("AUTH", auth)}
-          <ul>
-            <li>A dummy list</li>
-            <li>b</li>
-            <li>c</li>
-            <li>d</li>
-            <li>e</li>
-            <li>f</li>
-            <li>g</li>
-            <li>h</li>
-            <li>i</li>
-            <li>j</li>
-          </ul>
-        </div>
-      </>
-    );
+  return (
+    <>
+      <div>
+        <ul>
+          <li>A dummy list</li>
+          <li>b</li>
+          <li>c</li>
+          <li>d</li>
+          <li>e</li>
+          <li>f</li>
+          <li>g</li>
+          <li>h</li>
+          <li>i</li>
+          <li>j</li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Home;
