307eea95c0cddc2d404b8901a32bcb68bce77322
joshkat <joshua02k@gmail.com>
2023-08-22 11:38:28 -0400

    reformatted filed with prettier removed redundant imports

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,29 +1,32 @@
-import './css/App.css';
-import React, {useState, useEffect, Component} from "react";
-import{
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom';
-import SignIn from './Routes/SignIn';
+import "./css/App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import SignIn from "./Routes/SignIn";
 
-function App(){
-    return(
-      <Router>
-        <div className="App">
-          <div style={{margin:'0',display:'flex', justifyContent:'space-between',width:'100%', alignItems:'center',backgroundColor: 'red'}}>
-            <p>Shopping Center</p>
-            <div className='sections'>
-              <Link to= '/SignIn'>Sign In</Link>
-            </div>
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div
+          style={{
+            margin: "0",
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%",
+            alignItems: "center",
+            backgroundColor: "red",
+          }}
+        >
+          <p>Shopping Center</p>
+          <div className="sections">
+            <Link to="/SignIn">Sign In</Link>
           </div>
-          <Routes>
-            <Route exact path='/SignIn' element={<SignIn />}></Route>
-          </Routes>
         </div>
-      </Router>
-    )
+        <Routes>
+          <Route exact path="/SignIn" element={<SignIn />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- front_end/src/Routes/SignIn.jsx
+++ front_end/src/Routes/SignIn.jsx
@@ -1,23 +1,18 @@
-import React, {useState, useEffect, Component} from "react";
-import{
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom';
-function SignIn(){
-    return(
+function SignIn() {
+  return (
     <>
-        <h1>Sign In</h1>
+      <h1>Sign In</h1>
       <form>
         <label>Username</label>
-        <input type="text" name="username" id="username"></input><br />
+        <input type="text" name="username" id="username"></input>
+        <br />
         <label>Password</label>
-        <input type="text" name="password" id="password"></input><br />
+        <input type="text" name="password" id="password"></input>
+        <br />
         <button type="submit">Sign In</button>
       </form>
     </>
-    )
+  );
 }
 
 export default SignIn;
