ea0407976b75ac748a43e7f3731cbc4ae4817d4c
MartinLahoumh <martinlahoumh103102@gmail.com>
2023-08-22 22:20:55 -0400

    Revert "crash"
    This reverts commit c339028541806dc7a59ff383cb5fb9b5b536902c.

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -4,15 +4,21 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import SignIn from "./Routes/SignIn";
 import Home from "./Routes/Home";
 import ErrorPage from "./Routes/ErrorPage";
-import Header from "./components/Header";
+
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header auth={null} />
+        <header>
+          {/* replace p with logo */}
+          <p>Shopping Center</p>
+          <div className="sections">
+            {/* place everything else in header into here */}
+            <Link to="/SignIn">Sign In</Link>
+          </div>
+        </header>
         <Routes>
           <Route exact path="/SignIn" element={<SignIn />}></Route>
-          <Route path="/:auth" element={<Home />}></Route>
           <Route path="/" element={<Home />}></Route>
           <Route path="*" element={<ErrorPage />}></Route>
         </Routes>
