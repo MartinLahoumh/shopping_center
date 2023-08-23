1aef626f2b53217a9e277167af5490a3d11f0fbc
MartinLahoumh <martinlahoumh103102@gmail.com>
2023-08-22 23:08:01 -0400

    Revert "crash"
    This reverts commit 1cefc252b2c6c21de648639a606a3a150e4eb952.

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -4,17 +4,22 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import SignIn from "./Routes/SignIn";
 import Home from "./Routes/Home";
 import ErrorPage from "./Routes/ErrorPage";
-import SignUp from "./Routes/SignUp";
+
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1>Hello</h1>
+        <header>
+          {/* replace p with logo */}
+          <p>Shopping Center</p>
+          <div className="sections">
+            {/* place everything else in header into here */}
+            <Link to="/SignIn">Sign In</Link>
+          </div>
+        </header>
         <Routes>
           <Route exact path="/SignIn" element={<SignIn />}></Route>
-          <Route exact path="/SignUp" element={<Signup />}></Route>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/:auth" element={<Home />}></Route>
           <Route path="*" element={<ErrorPage />}></Route>
         </Routes>
       </div>
--- front_end/src/Routes/SignIn.jsx
+++ front_end/src/Routes/SignIn.jsx
@@ -1,37 +1,4 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Link } from 'react-router-dom';
-import Redirect from 'react-router-dom';
-
 function SignIn() {
-    const [authForm, setAuthForm] = useState({
-        username: "",
-        password: "",
-    })
-    const config = {headers:{'Content-Type':'application/json'}}
-    const handleChange = async(event)=>{
-        let value = event.target.value;
-        const fieldName = event.target.id;
-        setAuthForm((prevData)=>({...prevData, [fieldName]:value}));
-    }
-
-    const handleSubmit = async(event)=>{
-        event.preventDefault();
-        try {
-            const response = await axios.post(`http://localhost:5000/SignIn/${authForm.username}`,config)
-            if(response.data["msg"] == 1){
-                alert("Username not found");
-            } 
-            else if(response.data["msg"] == 2){
-                alert("Incorrect password");
-            }
-            else{
-                return(<Redirect to={"/"+response.access_token} />)
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
   return (
     <>
       <div>
@@ -44,7 +11,6 @@ function SignIn() {
           <input type="text" name="password" id="password"></input>
           <br />
           <button type="submit">Sign In</button>
-          <p>Dont have an account?</p><Link to="/SignUp">Sign Up!</Link>
         </form>
       </div>
     </>
