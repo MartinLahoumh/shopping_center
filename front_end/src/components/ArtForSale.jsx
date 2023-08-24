9af9adfab9d56052bab4aa39d77f3d946824dad6
joshkat <joshua02k@gmail.com>
2023-08-24 12:44:22 -0400

    generalized values & prettier reformat

--- front_end/src/components/ArtForSale.jsx
+++ front_end/src/components/ArtForSale.jsx
@@ -1,36 +1,44 @@
-import QuantityButton from './QuantityButton';
-import {useNavigate} from 'react-router-dom';
+import QuantityButton from "./QuantityButton";
+import { useNavigate } from "react-router-dom";
 
-function ArtForSale(){
+function ArtForSale() {
+  // assume pulled from DB using
+  const info = {
+    name: "Michael Jackson",
+    credit: "Andy Warhol, 1984",
+    description: "Description of the Art!",
+    imgURL: "https://ids.si.edu/ids/deliveryService?id=NPG-86T0028A_2&max=500",
+    price: 69,
+    url: "asdf", //a unique id
+  };
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const navigateToCheckout = () => {
+    // 👇️ navigate to /
+    navigate("../test2");
+  };
 
-    const navigateToCheckout = () => {
-        // 👇️ navigate to /
-        navigate("../test2");
-      };
+  return (
+    <>
+      <div className="artWrapper">
+        <div className="art pop-out">
+          <img src={info.imgURL} alt={info.description} />
+        </div>
 
-    return(
-        <>
-            <div className="artWrapper">
-                <div className="art pop-out">
-                    <img src="https://ids.si.edu/ids/deliveryService?id=NPG-86T0028A_2&max=500" alt="#" />
-                </div>
-
-                <div className="descriptionOfArt">
-                    <h1>Michael Jackson</h1>
-                    <h3>Credit: Andy Warhol, 1984.</h3>
-                    <p>
-                        Description of the Art!
-                    </p>
-                </div>
-                <div className='purchaseArt'>
-                    <p>Quantity:</p>
-                    <QuantityButton></QuantityButton>
-                    <button className="artButton" onClick={navigateToCheckout}>Buy</button>
-                </div>
-            </div>
-        </>
-    );
+        <div className="descriptionOfArt">
+          <h1>{info.name}</h1>
+          <h3>Credit: {info.credit}</h3>
+          <p>{info.description}</p>
+        </div>
+        <div className="purchaseArt">
+          <p>Quantity:</p>
+          <QuantityButton></QuantityButton>
+          <button className="artButton" onClick={navigateToCheckout}>
+            Buy
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 export default ArtForSale;
