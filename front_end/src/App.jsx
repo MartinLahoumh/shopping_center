02a23c864ab9d5b5b1a0939bf01d6e8cdd82b950
joshkat <joshua02k@gmail.com>
2023-08-24 14:34:36 -0400

    Removed ".App" div Useless div that had no effect on look of app Removed CSS which targeted this div

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -10,26 +10,24 @@ import CheckoutPage from "./Routes/CheckOutPage";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header>
-          {/* replace p with logo */}
-          <Link to="/">
-            <p>Shopping Center</p>
-          </Link>
-          <div className="sections">
-            {/* place everything else in header into here */}
-            <Link to="/SignIn">Sign In</Link>
-            <Link to="/test2">CheckOut</Link>
-          </div>
-        </header>
-        <Routes>
-          <Route exact path="/SignIn" element={<SignIn />}></Route>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/ArtPage" element={<ArtPage />}></Route>
-          <Route path="/test2" element={<CheckoutPage />}></Route>
-          <Route path="*" element={<ErrorPage />}></Route>
-        </Routes>
-      </div>
+      <header>
+        {/* replace p with logo */}
+        <Link to="/">
+          <p>Shopping Center</p>
+        </Link>
+        <div className="sections">
+          {/* place everything else in header into here */}
+          <Link to="/SignIn">Sign In</Link>
+          <Link to="/test2">CheckOut</Link>
+        </div>
+      </header>
+      <Routes>
+        <Route exact path="/SignIn" element={<SignIn />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/ArtPage" element={<ArtPage />}></Route>
+        <Route path="/test2" element={<CheckoutPage />}></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
+      </Routes>
     </Router>
   );
 }
