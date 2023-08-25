00605b0cda9c080c5df1bf21dafcfd159ade324d
Pablo Lara <pablolara01@yahoo.com>
2023-08-25 12:16:02 -0400

    Updated Home jsx with a home.css stylesheet, and update to Quantity Button.css

--- front_end/src/Routes/Home.jsx
+++ front_end/src/Routes/Home.jsx
@@ -1,16 +1,17 @@
 import { useParams } from "react-router";
 import TrendingSlider from "../components/TrendingSlider";
 import DisplaySection from "../components/DisplaySection";
+import "../css/Home.css";
 
 function Home() {
   let { auth } = useParams();
   return (
     <>
-      <div>
+      <div className="homeContent">
         {console.log("AUTH", auth)}
         <TrendingSlider />
-        <DisplaySection sectionName="Test Section" />
-        <DisplaySection sectionName="Test Section 2" />
+        <DisplaySection sectionName="Pop" />
+        <DisplaySection sectionName="Photography" />
       </div>
     </>
   );
