789ca8f8e5c294ecab1a1b843cba8bc980ef0f57
Josh <joshua02k@gmail.com>
2023-08-25 15:14:36 -0400

    Removed CSS import

--- front_end/src/Routes/Home.jsx
+++ front_end/src/Routes/Home.jsx
@@ -1,7 +1,6 @@
 import { useParams } from "react-router";
 import TrendingSlider from "../components/TrendingSlider";
 import DisplaySection from "../components/DisplaySection";
-import "../css/Home.css";
 
 function Home() {
   let { auth } = useParams();
