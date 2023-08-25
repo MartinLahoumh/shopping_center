ec1b948305084882c458cc0952531c8fe5518618
Pablo Lara <pablolara01@yahoo.com>
2023-08-25 11:44:51 -0400

    Updates to Art Page jsx & css files. Also made a css file for Quantity Button, and added a new font style in the index.css

--- front_end/src/Routes/ArtPage.jsx
+++ front_end/src/Routes/ArtPage.jsx
@@ -28,7 +28,7 @@ export default function ArtPage() {
     setInfo({
       name: "Michael Jackson",
       credit: "Andy Warhol, 1984",
-      description: "Description of the Art!",
+      description: "Portrait of Jackson commissioned by Andy Warhal for the cover of Time Magazin's issue for March 1984.",
       imgURL:
         "https://ids.si.edu/ids/deliveryService?id=NPG-86T0028A_2&max=500",
       price: 69,
@@ -48,7 +48,7 @@ export default function ArtPage() {
             <p>{info.description}</p>
           </div>
           <div className="purchaseArt">
-            <p>Quantity:</p>
+            <p>Quantity: </p>
             <QuantityButton quantity={quantity} setQuantity={setQuantity} />
             <button className="artButton" onClick={navigateToCheckout}>
               Buy
--- front_end/src/components/QuantityButton.jsx
+++ front_end/src/components/QuantityButton.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import "../css/QuantityButton.css";
 
 function QuantityButton({ quantity, setQuantity }) {
   const qtyIncrement = () => {
