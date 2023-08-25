f56d11ac0be542b6f6853629722fbbb0b515dbc7
Josh <joshua02k@gmail.com>
2023-08-24 22:32:31 -0400

    Fixed header and Cleaned up App.jsx App.jsx /:auth route was not needed and broke * page Header now supports logout via localStorage

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import SignIn from "./Routes/SignIn";
 import SignUp from "./Routes/SignUp";
+import Post from "./Routes/Post";
 import Home from "./Routes/Home";
 import ErrorPage from "./Routes/ErrorPage";
 import ArtPage from "./Routes/ArtPage";
 import CheckoutPage from "./Routes/CheckOutPage";
+
 import Header from "./components/Header";
 
 function App() {
@@ -14,13 +16,13 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route exact path="/SignIn" element={<SignIn />}></Route>
-        <Route exact path="/SignUp" element={<SignUp />}></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/:auth" element={<Home />}></Route>
+        <Route path="/SignIn" element={<SignIn />}></Route>
+        <Route path="/SignUp" element={<SignUp />}></Route>
+        <Route path="/Post" element={<Post />}></Route>
         <Route path="/ArtPage" element={<ArtPage />}></Route>
         <Route path="/test2" element={<CheckoutPage />}></Route>
-        <Route path="*" element={<ErrorPage />}></Route>
       </Routes>
     </Router>
   );
--- front_end/src/components/Header.jsx
+++ front_end/src/components/Header.jsx
@@ -1,25 +1,33 @@
 import "../css/App.css";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-import SignIn from "../Routes/SignIn";
-import Home from "../Routes/Home";
+function Header() {
+  const [userId, setUserId] = useState(null);
 
-function Header(props) {
+  useEffect(() => {
+    setUserId(localStorage.getItem("user_id"));
+    console.log(localStorage.getItem("user_id"));
+  }, [userId]);
+
+  function handleLogOut() {
+    localStorage.removeItem("user_id");
+    window.location.reload();
+  }
+
   return (
     <header>
-      {/* replace p with logo */}
-      <Link to="/" element={<Home />}>
+      <Link to="/">
         <p>Shopping Center</p>
       </Link>
       <div className="sections">
-        {/* place everything else in header into here */}
-        {props.auth == null && (
-          <Link to="/SignIn" element={<SignIn />}>
-            Sign In
-          </Link>
-        )}
-        {props.auth != null && <Link to="/SignIn">Post</Link> && (
-          <Link to="/SignIn">Log Out</Link>
+        {userId === null ? (
+          <Link to="/SignIn">Sign In</Link>
+        ) : (
+          <>
+            <Link to="/Post">Post</Link>
+            <button onClick={handleLogOut}>Log Out</button>
+          </>
         )}
       </div>
     </header>
